0166e4465b3016ba4d92221af579b0d0a008972f
Dione de Souza Silva <dione2508@gmail.com>
2024-06-23 21:03:04 -0300

    Primeira aula.

---
+++ mono/travel-app/src/main/java/com/dionesouza/hotel/Hotel.java
@@ -0,0 +1,12 @@
+package com.dionesouza.hotel;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Hotel extends PanacheEntity{
+    
+    public Long travelOrderId;
+    public int nights;
+
+}
